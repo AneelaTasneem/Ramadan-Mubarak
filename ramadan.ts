4074c0451ae137c286777887ae331e60a14b3283
AneelaTasneem <aneelatasneem90@gmail.com>
2024-03-15 07:49:25 -0700

    first commit

---
+++ ramadan.ts
@@ -0,0 +1,11 @@
+
+// RAMADAN MUBARAK:
+
+import chalk from "chalk";
+import { clearScreenDown } from "readline";
+
+console.log(chalk.yellow("May this Ramadan"));
+console.log(chalk.red ("heals ")   +   chalk.yellow("our hearts"));
+console.log(chalk.red("Cleanses ")  +   chalk.yellow( "our minds"));
+console.log(chalk.red (" Purifies ")    +   chalk.yellow( "our souls"));
+console.log(chalk.green (" RAMADAN MUBARAK"));
